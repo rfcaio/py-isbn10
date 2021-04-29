4f4464a99acfa66891162c575cfef4474aaa7f65
Caio Rodrigues <rfcaio90@gmail.com>
2021-04-29 14:31:56 -0300

    create a function to validate a ISBN-10 code

--- isbn10.py
+++ isbn10.py
@@ -1,3 +1,8 @@
+def is_valid_isbn10(isbn10):
+    isbn10_checksum = get_isbn10_checksum(isbn10)
+    return isbn10_checksum % 11 == 0
+
+
 def get_isbn10_checksum(isbn10):
     result = 0
     for index, digit_string in enumerate(isbn10):
--- test_isbn10.py
+++ test_isbn10.py
@@ -1,9 +1,16 @@
 import unittest
 
-from isbn10 import get_isbn10_checksum
+from isbn10 import get_isbn10_checksum, is_valid_isbn10
 
 
 class ISBN10Test(unittest.TestCase):
     def test_get_isbn10_checksum(self):
         self.assertEqual(get_isbn10_checksum('1932698183'), 264)
         self.assertEqual(get_isbn10_checksum('855080603X'), 253)
+
+    def test_is_valid_isbn10(self):
+        self.assertFalse(is_valid_isbn10('1932698184'))
+        self.assertFalse(is_valid_isbn10('8550806030'))
+
+        self.assertTrue(is_valid_isbn10('1932698183'))
+        self.assertTrue(is_valid_isbn10('855080603X'))
