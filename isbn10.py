358bccf8f513a6440d2cc524ed71a15f28934479
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-04 21:08:26 -0300

    remove old functions in the codebase

--- isbn10.py
+++ isbn10.py
@@ -36,32 +36,3 @@ class ISBN10:
             coefficient = 10 - index
             result += coefficient * digit
         return result
-
-
-def format_isbn10(isbn10):
-    if not is_valid_isbn10(isbn10):
-        raise InvalidISBN10Error('Invalid ISBN-10 code.')
-
-    isbn10_match = re.match(ISBN10_DIGIT_GROUPS, isbn10)
-    return isbn10_match.expand(ISBN10_FORMAT)
-
-
-def is_valid_isbn10(isbn10):
-    if not is_valid_isbn10_format(isbn10):
-        return False
-
-    isbn10_checksum = get_isbn10_checksum(isbn10)
-    return isbn10_checksum % 11 == 0
-
-
-def is_valid_isbn10_format(isbn10):
-    return type(isbn10) == str and re.match(VALID_ISBN10_FORMAT, isbn10)
-
-
-def get_isbn10_checksum(isbn10):
-    result = 0
-    for index, digit_string in enumerate(isbn10):
-        digit = 10 if digit_string == 'X' else int(digit_string)
-        coefficient = 10 - index
-        result += coefficient * digit
-    return result
--- test_isbn10.py
+++ test_isbn10.py
@@ -1,13 +1,6 @@
 import unittest
 
-from isbn10 import (
-    format_isbn10,
-    get_isbn10_checksum,
-    is_valid_isbn10,
-    is_valid_isbn10_format,
-    InvalidISBN10Error,
-    ISBN10
-)
+from isbn10 import InvalidISBN10Error, ISBN10
 
 
 class ISBN10Test(unittest.TestCase):
@@ -59,30 +52,3 @@ class ISBN10Test(unittest.TestCase):
 
         isbn_with_final_x = ISBN10('855080603X')
         self.assertEqual(str(isbn_with_final_x), '85-508-0603-X')
-
-    def test_format_isbn10(self):
-        with self.assertRaises(InvalidISBN10Error):
-            format_isbn10('193269818X')
-
-        self.assertEqual(format_isbn10('1932698183'), '19-326-9818-3')
-        self.assertEqual(format_isbn10('855080603X'), '85-508-0603-X')
-
-    def test_get_isbn10_checksum(self):
-        self.assertEqual(get_isbn10_checksum('1932698183'), 264)
-        self.assertEqual(get_isbn10_checksum('855080603X'), 253)
-
-    def test_is_valid_isbn10(self):
-        self.assertFalse(is_valid_isbn10('1932698184'))
-        self.assertFalse(is_valid_isbn10('8550806030'))
-        self.assertFalse(is_valid_isbn10(1932698183))
-        self.assertFalse(is_valid_isbn10('855080603x'))
-
-        self.assertTrue(is_valid_isbn10('1932698183'))
-        self.assertTrue(is_valid_isbn10('855080603X'))
-
-    def test_is_valid_isbn10_format(self):
-        self.assertFalse(is_valid_isbn10_format(1932698183))
-        self.assertFalse(is_valid_isbn10_format('855080603x'))
-
-        self.assertTrue(is_valid_isbn10_format('1932698183'))
-        self.assertTrue(is_valid_isbn10_format('855080603X'))
